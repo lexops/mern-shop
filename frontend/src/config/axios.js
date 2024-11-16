9033b336b2a53db6e7e2c0acb1cef855ed9cd7d7
lexops <134613820+lexops@users.noreply.github.com>
2024-11-16 11:27:12 -0300

    style(frontend): lint

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,6 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
+// Create the Axios instance
 export const axiosi = axios.create({
   withCredentials: true,
   baseURL: process.env.REACT_APP_BASE_URL
-})
+});

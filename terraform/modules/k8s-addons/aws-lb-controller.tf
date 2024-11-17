894bc7cf367f624c47fde7673c52b8367d927cc1
lexops <134613820+lexops@users.noreply.github.com>
2024-11-17 14:53:45 -0300

    create alb

--- terraform/modules/k8s-addons/aws-lb-controller.tf
+++ terraform/modules/k8s-addons/aws-lb-controller.tf
@@ -1,57 +1,57 @@
-# module "load_balancer_controller_irsa_role" {
-#   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+module "load_balancer_controller_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-#   role_name                              = "load-balancer-controller"
-#   attach_load_balancer_controller_policy = true
+  role_name                              = "load-balancer-controller"
+  attach_load_balancer_controller_policy = true
 
-#   oidc_providers = {
-#     eks = {
-#       provider_arn               = var.eks_oidc_provider_arn
-#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-#     }
-#   }
-# }
+  oidc_providers = {
+    eks = {
+      provider_arn               = var.eks_oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
 
-# resource "helm_release" "aws_loadbalancer_controller" {
-#   namespace  = "kube-system"
-#   name       = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   version    = "1.10.0"
+resource "helm_release" "aws_loadbalancer_controller" {
+  namespace  = "kube-system"
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.10.0"
 
-#   set {
-#     name  = "clusterName"
-#     value = var.eks_cluster_name
-#   }
+  set {
+    name  = "clusterName"
+    value = var.eks_cluster_name
+  }
 
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "true"
-#   }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = module.load_balancer_controller_irsa_role.iam_role_arn
-#   }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.load_balancer_controller_irsa_role.iam_role_arn
+  }
 
-#   set {
-#     name  = "tolerations[0].key"
-#     value = "CriticalAddonsOnly"
-#   }
+  set {
+    name  = "tolerations[0].key"
+    value = "CriticalAddonsOnly"
+  }
 
-#   set {
-#     name  = "tolerations[0].operator"
-#     value = "Exists"
-#   }
+  set {
+    name  = "tolerations[0].operator"
+    value = "Exists"
+  }
 
-#   set {
-#     name  = "tolerations[0].effect"
-#     value = "NoSchedule"
-#   }
+  set {
+    name  = "tolerations[0].effect"
+    value = "NoSchedule"
+  }
 
-# }
+}

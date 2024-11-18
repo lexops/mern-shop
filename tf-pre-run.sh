d3a6c8871c85064604193ed9bbd2cdc06796a02c
lexops <134613820+lexops@users.noreply.github.com>
2024-11-18 09:35:52 -0300

    improve pre run script

--- tf-pre-run.sh
+++ tf-pre-run.sh
@@ -4,8 +4,8 @@ apt-get update
 apt-get install -y unzip
 
 echo "Installing awscli ..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
 ./aws/install
 
 echo "Installing kubectl ..."
